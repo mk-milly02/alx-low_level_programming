a96147e9d6d54476aab9bd70d4e4bf85c9558266
Jonas Ababio <jonasagesmilly@outlook.com>
2023-06-15 09:34:08 -0700

    Hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-	char* text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	int fd = open("/dev/stderr", O_WRONLY);
 
